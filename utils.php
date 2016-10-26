80de7e71191594360998d48c18d508348a2af909
Aaron Holbrook <aaron@zeek.com>
2016-10-26 05:54:50 -0500

    Add docblocks for functions

--- utils.php
+++ utils.php
@@ -2,6 +2,15 @@
 
 namespace Zeek\Util;
 
+/**
+ * Reads an array safely for requested indice
+ * Does not throw notices, instead simply returns an empty string if the value does not exist
+ *
+ * @param array $array
+ * @param string $name
+ *
+ * @return string
+ */
 function safe_read( $array, $name ) {
 
 	if ( empty( $array[ $name ] ) ) {
@@ -11,6 +20,16 @@ function safe_read( $array, $name ) {
 	return $array[ $name ];
 }
 
+/**
+ * Checks for and returns a term by the slug
+ *
+ * Initializes the term if it does not yet exist
+ *
+ * @param string $slug
+ * @param string $taxonomy
+ *
+ * @return bool
+ */
 function init_term( $slug, $taxonomy ) {
 
 	if ( ! function_exists( 'get_term_by' ) || ! function_exists( 'wp_insert_term' ) ) {
@@ -26,6 +45,15 @@ function init_term( $slug, $taxonomy ) {
 	return wp_insert_term( $slug, $taxonomy );
 }
 
+/**
+ * Checks that a constant is defined and that it is true
+ *
+ * Returns false if the constant does not exist or if it is not true
+ *
+ * @param string $constant
+ *
+ * @return bool
+ */
 function is_constant_true( $constant ) {
 	if ( ! defined( $constant ) ) {
 		return false;
