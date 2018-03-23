9652194c1324165299ff44c7fb5ffb0723c6b5c2
Aaron Holbrook <aaron@zeek.com>
2018-03-23 07:38:29 -0500

    Update phpunit to 7

--- tests/safeReadTest.php
+++ tests/safeReadTest.php
@@ -2,13 +2,9 @@
 
 namespace Zeek\PHP_Util;
 
-// backward compatibility
-if ( ! class_exists( '\PHPUnit\Framework\TestCase' ) &&
-     class_exists( '\PHPUnit_Framework_TestCase' ) ) {
-	class_alias( '\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase' );
-}
+use PHPUnit\Framework\TestCase;
 
-class UtilsTest extends \PHPUnit_Framework_TestCase {
+class UtilsTest extends TestCase {
 
 	public function testSafeReadDefined() {
 
