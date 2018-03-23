e4d9769327199c6f8e3474db09b9292fc94c28f2
Aaron Holbrook <aaron@zeek.com>
2018-03-23 07:24:43 -0500

    Add tests for saferead

---
+++ tests/safeReadTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Zeek\PHP_Util;
+
+class UtilsTest extends \PHPUnit_Framework_TestCase {
+
+	public function testSafeReadDefined() {
+
+		$this->assertEquals( true, function_exists( 'Zeek\PHP_Util\safe_read' ) );
+	}
+
+	public function testSafeReadCanReadExistingIndice() {
+		$array_to_test = [
+			'index1' => 'somevalue',
+		];
+
+		$this->assertEquals( 'somevalue', safe_read( $array_to_test, 'index1' ) );
+	}
+
+	public function testSafeReadReturnsEmptyStringOnInvalidKey() {
+		$array_to_test = [
+			'index1' => 'somevalue',
+		];
+
+		$this->assertEquals( '', safe_read( $array_to_test, 'index2' ) );
+	}
+
+	/**
+	 * @expectedException \Error
+	 */
+	public function testSafeReadDoesntHandleObjects() {
+		$object_to_test = new stdClass();
+		$object_to_test->index1 = 'somevalue';
+
+		safe_read( $object_to_test, 'index1' );
+	}
+
+	public function testSafeReadHandlesNull() {
+		$this->assertEquals( '', safe_read( null, 'index1' ) );
+	}
+
+	public function testSafeReadHandlesEmptyString() {
+		$this->assertEquals( '', safe_read( '', 'index1' ) );
+	}
+
+}
